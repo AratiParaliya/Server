e800216b0c20738b441f29220bc06e2e38e40340
Arati <aparaliya757@rku.ac.in>
2026-04-18 17:58:15 +0530

    update banner

--- routes/banner.js
+++ routes/banner.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Banner = require("../models/banner");
-
-
-
-
+const mongoose = require("mongoose");
 
 const pLimit = require('p-limit');
 
